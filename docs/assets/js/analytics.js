5c0e9b6dcb6a406ca6ce8fae07b4b0405cae5e5f
OffloveProject <offlove.project@gmail.com>
2026-06-04 15:28:45 +0900

    Add files via upload

--- docs/assets/js/analytics.js
+++ docs/assets/js/analytics.js
@@ -1,16 +1,20 @@
 (function () {
-  // Google Analyticsを使用する場合は、下記に測定IDを設定してください。
-  // 例: window.OFFSTAGE_LOVE_GA_ID = "G-XXXXXXXXXX";
-  window.OFFSTAGE_LOVE_GA_ID = window.OFFSTAGE_LOVE_GA_ID || "";
-  const id = window.OFFSTAGE_LOVE_GA_ID;
-  if (!id) return;
-  const script = document.createElement("script");
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(id)}`;
-  document.head.appendChild(script);
+  var GA_MEASUREMENT_ID = window.OFFSTAGE_LOVE_GA_ID || "G-KM4SFPPFDT";
+  if (!GA_MEASUREMENT_ID) return;
+
   window.dataLayer = window.dataLayer || [];
-  function gtag(){ window.dataLayer.push(arguments); }
-  window.gtag = gtag;
-  gtag("js", new Date());
-  gtag("config", id);
+  window.gtag = window.gtag || function () {
+    window.dataLayer.push(arguments);
+  };
+
+  var alreadyLoaded = document.querySelector('script[src*="googletagmanager.com/gtag/js"]');
+  if (!alreadyLoaded) {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(GA_MEASUREMENT_ID);
+    document.head.appendChild(script);
+  }
+
+  window.gtag("js", new Date());
+  window.gtag("config", GA_MEASUREMENT_ID);
 })();
